4fbb43b8f8a5738837636042e0c3b77718563faa
“OjoOluwagbenga700” <ojooluwagbenga700@gmail.com>
2025-05-26 16:20:57 +0100

    update with API Gateway

--- Infra/main.tf
+++ Infra/main.tf
@@ -11,8 +11,30 @@ module "iam" {
   table_name = var.table_name
 }
 
-module "lambda" {
+module "lambda_primary" {
   source          = "./modules/lambda"
   table_name      = var.table_name
   lambda_role_arn = module.iam.lambda_role_arn
+  providers       = { aws = aws.primary }
+
+}
+
+module "lambda_secondary" {
+  source          = "./modules/lambda"
+  table_name      = var.table_name
+  lambda_role_arn = module.iam.lambda_role_arn
+  providers       = { aws = aws.secondary }
+
+}
+
+module "apigateway_primary" {
+  source                   = "./modules/apigateway"
+  providers                = { aws = aws.primary }
+  dr_functions_invoke_arns = module.lambda_primary.dr_functions_invoke_arns
+}
+
+module "apigateway_secondary" {
+  source                   = "./modules/apigateway"
+  providers                = { aws = aws.secondary }
+  dr_functions_invoke_arns = module.lambda_secondary.dr_functions_invoke_arns
 }
---
+++ Infra/modules/apigateway/main.tf
@@ -0,0 +1,48 @@
+locals {
+  api_routes = {
+    "GET /read"                  = "read_function"
+    "POST /write"                 = "write_function"
+  }
+}
+
+
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "api_lambda_integrations" {
+  for_each = local.api_routes
+
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.dr_functions_invoke_arns[each.value]
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "routes" {
+  for_each = local.api_routes
+
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = each.key
+  target    = "integrations/${aws_apigatewayv2_integration.api_lambda_integrations[each.key].id}"
+}
+
+
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gateway_permissions" {
+  for_each = local.api_routes
+
+  statement_id  = "AllowAPIGatewayInvoke-${each.value}"
+  action        = "lambda:InvokeFunction"
+  function_name = var.dr_functions_invoke_arns[each.value]
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
