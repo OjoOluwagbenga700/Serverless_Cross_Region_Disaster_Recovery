6b22f107f3cfa1182b1c8c30395e9b711e14becc
“OjoOluwagbenga700” <ojooluwagbenga700@gmail.com>
2025-06-02 12:02:59 +0100

    updated

--- frontend/variable.tf
+++ frontend/variable.tf
@@ -1,10 +1,10 @@
 variable "bucket_name" {
-    description = "Name of the S3 bucket"
-    type        = string
-    default     = "frontend-bucket-007"
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "frontend-bucket-007"
 }
 variable "region" {
-    description = "AWS region"
-    type        = string
-    default     = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
